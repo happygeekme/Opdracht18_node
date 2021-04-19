b11d8d0714ad3de0625d4705567483f8ecba0860
Jitske <80577121+happygeekme@users.noreply.github.com>
2021-04-19 16:04:57 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("Hallo Winc Academy Studenten")
+
+//dit is een grote som (je ziet dit niet want is een comment)
+/*let multiplication = 1230941 * 1823794;
+console.log("multiplication:", multiplication);
+
+let division = 637263 / 54;
+console.log("division:", division);
+Nu zie je dit ook niet meer, uitgecomment
+*/
